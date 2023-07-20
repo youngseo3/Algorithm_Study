39a0efe3169321b0348c70a94bbb9361182ce101
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-07-20 20:00:59 +0900

    [PG_120804] 두 수의 곱

---
+++ SeungeEEE_Java/src/week_01/PG_120804.java
@@ -0,0 +1,24 @@
+package week_01;
+
+public class PG_120804 {
+	public static void main(String[] args) {
+		int num1 = 3;
+		int num2 = 4;
+		int answer = 0;
+		
+		answer = num1 * num2;
+		
+		System.out.println(answer);
+		
+	}
+}
+
+/* class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        answer = num1 * num2;
+        
+        return answer;
+    }
+} */

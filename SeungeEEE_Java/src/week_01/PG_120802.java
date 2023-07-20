60c8c474de13ce0eadb249059963cc059e5bf978
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-07-20 19:51:21 +0900

    [PG_120802] 두 수의 합

---
+++ SeungeEEE_Java/src/week_01/PG_120802.java
@@ -0,0 +1,24 @@
+package week_01;
+
+
+class PG_120802 {
+    public static void main(String[] args) {
+        int num1 = 2; 
+        int num2 = 3;
+        int answer = -1;
+        
+        answer = num1 + num2;
+        
+        System.out.println(answer);
+    }
+}
+// 프로그래머스 
+/* class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        
+        answer = num1 + num2;
+        
+        return answer;
+    }
+} */

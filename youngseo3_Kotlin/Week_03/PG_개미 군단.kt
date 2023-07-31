b0b89d53311ea35b443154ee1eaf7de6b2147bfe
youngseo3 <wys4475@gmail.com>
2023-07-31 20:34:49 +0900

    [PG-120837] 개미 군단

---
+++ youngseo3_Kotlin/Week_03/PG_개미 군단.kt
@@ -0,0 +1,11 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_03
+
+class Solution {
+    fun solution(hp: Int): Int {
+        val f = hp / 5
+        val t = (hp % 5) / 3
+        val o = (hp % 5) % 3
+        var answer = f + t + o
+        return answer
+    }
+}

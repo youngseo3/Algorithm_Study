fac17f8da7338ff67cb032257da4e55e2c139655
youngseo3 <wys4475@gmail.com>
2023-08-14 17:03:46 +0900

    [PG-120842] 2차원으로 만들기

---
+++ youngseo3_Kotlin/Week_04/PG_2차원으로 만들기.kt
@@ -0,0 +1,29 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_04
+
+
+//class Solution { 다른 사람 풀이
+//    fun solution(numList: IntArray, n: Int): Array<IntArray> {
+//        val answer = Array(numList.size / n) { IntArray(n) }
+//        var count = 0
+//
+//        for (i in answer.indices) {
+//            for (j in answer[i].indices) {
+//                answer[i][j] = numList[count]
+//                count++
+//            }
+//        }
+//        return answer
+//    }
+//}
+class Solution {
+    fun solution(num_list: IntArray, n: Int): List<List<Int>> {
+        var answer = num_list.toList().chunked(n)
+        return answer
+
+    }
+}
+
+fun main() {
+    var s = Solution()
+    print(s.solution((0..8).toList().toIntArray(), 3))
+}

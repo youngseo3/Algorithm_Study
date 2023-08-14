a951045896503f1b98dc2814d8aaaca16d5204eb
youngseo3 <wys4475@gmail.com>
2023-08-14 17:37:02 +0900

    [PG-120843] 공 던지기

---
+++ youngseo3_Kotlin/Week_04/PG_공 던지기.kt
@@ -0,0 +1,17 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_04
+
+
+//class Solution { 다른 사람 풀이
+//    fun solution(numbers: IntArray, k: Int): Int = numbers[(k - 1) * 2 % numbers.size]
+//}
+
+class Solution {
+    fun solution(numbers: IntArray, k: Int): Int {
+        var index = 0
+        for(i in 1..k-1) {
+            index += 2
+            if(numbers.size-1 < index) index -= numbers.size
+        }
+        return numbers[index]
+    }
+}

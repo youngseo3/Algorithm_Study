ac66168195a9563c16a8eb32ba41781fa3ff1fd7
youngseo3 <wys4475@gmail.com>
2023-08-15 00:28:37 +0900

    [PG-120847] 최댓값 만들기(1)

---
+++ youngseo3_Kotlin/Week_04/PG_최댓값 만들기(1).kt
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_04
+
+/* 주어진 정수 배열 numbers를 오름차순으로 정렬하고 가장 큰 두개의 숫자를 선택
+숫자를 fold함수를 사용하여 곱하는데 이때 초기값은 1, 곱한 값읇 반환 */
+//class Solution { // 다른 사람 풀이
+//    fun solution(numbers: IntArray): Int  = numbers.sorted().takeLast(2).fold(1) { i, acc -> acc * i }
+//}
+
+class Solution {
+    fun solution(numbers: IntArray): Int {
+        return numbers.sortedDescending()[0] * numbers.sortedDescending()[1]
+        numbers.sort()
+    }
+}

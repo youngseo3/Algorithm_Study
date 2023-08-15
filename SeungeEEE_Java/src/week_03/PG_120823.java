86dad6524021ef783857b6de8c23ba555ca3636a
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-08-15 23:59:15 +0900

    [PG_120823] 직각삼각형 출력하기

---
+++ SeungeEEE_Java/src/week_03/PG_120823.java
@@ -0,0 +1,34 @@
+package week_03;
+
+import java.util.Scanner;
+
+public class PG_120823 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+// 프로그래머스 
+/* import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} */

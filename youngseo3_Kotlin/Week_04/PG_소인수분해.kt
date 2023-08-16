0c644c2994aa9401357d6fab388a60a5f66e4d5a
youngseo3 <wys4475@gmail.com>
2023-08-16 17:38:23 +0900

    [PG-120852] 소인수분해

---
+++ youngseo3_Kotlin/Week_04/PG_소인수분해.kt
@@ -0,0 +1,15 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_04
+
+class Solution {
+    fun solution(n: Int): IntArray {
+        var answer = mutableSetOf<Int>() // Hashset사용해도 추가 가능함. -> 다만 순서 보장하지 않음
+        var result = n
+        for(i in 2..n) {
+            while(result % i == 0) {
+                answer.add(i)
+                result /= i
+            }
+        }
+        return answer.toIntArray()
+    }
+}

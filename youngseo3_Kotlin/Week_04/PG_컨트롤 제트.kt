9f9a9c5b3c987be2c653c883120350a3ba2d9085
youngseo3 <wys4475@gmail.com>
2023-08-16 18:22:15 +0900

    [PG-120853] 컨트롤 제트

---
+++ youngseo3_Kotlin/Week_04/PG_컨트롤 제트.kt
@@ -0,0 +1,39 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_04
+
+//import java.util.*
+//class Solution { // 다른 사람 풀이 -> Stack 사용해서 Z를 만나면 스택포인터가 가리키는 것을 삭제함
+//    fun solution(s: String): Int {
+//        var answer = 0
+//        val stack = Stack<Int>()
+//
+//        for (w in s.split(" ".toRegex())) {
+//            if (w == "Z") {
+//                stack.pop()
+//            } else {
+//                stack.push(w.toInt())
+//            }
+//        }
+//        for (i in stack) {
+//            answer += i
+//        }
+//        return answer
+//    }
+//}
+
+class Solution {
+    fun solution(s: String): Int {
+        var ary = s.split(" ")
+        var a = 0
+        for(i in ary.indices) {
+            if(ary[i].contains("[0-9]".toRegex())) a += ary[i].toString().toInt()
+            else if(i >=1) a -= ary[i-1].toString().toInt()
+            println(a)
+        }
+        return a
+    }
+}
+
+fun main() {
+    var s = Solution()
+    print(s.solution("10 20 30 40"))
+}

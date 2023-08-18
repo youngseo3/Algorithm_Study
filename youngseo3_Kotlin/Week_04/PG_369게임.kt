fa21c228c188ef0e61d13eac58deff7bd0b511f2
youngseo3 <wys4475@gmail.com>
2023-08-18 21:11:14 +0900

    [PG-120891] 369게임

---
+++ youngseo3_Kotlin/Week_04/PG_369게임.kt
@@ -0,0 +1,15 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_04
+
+//class Solution { // 다른 사람 풀이 -> 이게 더 좋은듯
+//    fun solution(order: Int): Int = order.toString().count { it == '3' || it == '6' || it == '9' }
+//}
+
+class Solution {
+    fun solution(order: Int): Int {
+        var count = 0
+        for(i in order.toString()) {
+            if(i == '3' || i == '6' || i == '9') count ++
+        }
+        return count
+    }
+}

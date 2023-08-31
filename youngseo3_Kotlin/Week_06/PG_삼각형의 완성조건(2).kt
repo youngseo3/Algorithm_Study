2623c9f79185d0b92cdbb53f483691a668f2fe48
youngseo3 <wys4475@gmail.com>
2023-08-31 21:20:15 +0900

    [PG-120868] 삼각형의 완성조건(2)

---
+++ youngseo3_Kotlin/Week_06/PG_삼각형의 완성조건(2).kt
@@ -0,0 +1,25 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_06
+
+//import kotlin.math.*
+//
+//class Solution { // 다른 사람 풀이 -> 합 - 차의 절댓값 - 1
+//    fun solution(sides: IntArray): Int {
+//        var high = sides[0] + sides[1]
+//        var low = abs(sides[0] - sides[1])
+//
+//        return high - low - 1
+//    }
+//}
+
+class Solution {
+    fun solution(sides: IntArray): Int {
+        var count = 0
+        val maxNum = sides.maxOrNull()!!
+        val minNum = sides.minOrNull()!!
+        for(i in maxNum - minNum + 1..maxNum)
+            count ++
+        for(i in maxNum + 1 until sides[0] + sides[1])
+            count ++
+        return count
+    }
+}

ce2f9b2eca10c64a74fc34e71b9aa970731ebf32
youngseo3 <wys4475@gmail.com>
2023-09-05 13:38:08 +0900

    [PG-120876] 겹치는 선분의 길이

---
+++ youngseo3_Kotlin/Week_06/PG_겹치는 선분의 길이.kt
@@ -0,0 +1,15 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Kotlin.Week_06
+
+class Solution { // 배열을 생성해서 각 인덱스의 값이 2 이상이면 겹치는 선분이라 판단.
+    fun solution(lines: Array<IntArray>): Int {
+        var answer: Int = 0
+        var arr = IntArray(201){0}
+        for(i in lines.indices){
+            for(j in lines[i][0]+1 .. lines[i][1]){
+                arr[j+100]++
+            }
+        }
+        answer += arr.count{e-> e >= 2}
+        return answer
+    }
+}

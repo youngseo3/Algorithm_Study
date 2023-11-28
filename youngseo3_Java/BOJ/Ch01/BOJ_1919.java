6c44360b942872b2dcf97c5ed3fac739ef848675
youngseo3 <wys4475@gmail.com>
2023-11-29 01:12:47 +0900

    [BOJ_1919] 애너그램 만들기

---
+++ youngseo3_Java/BOJ/Ch01/BOJ_1919.java
@@ -0,0 +1,27 @@
+package kr.ac.tukorea.Algorithm_Study.youngseo3_Java.BOJ.Ch01;
+
+import java.util.Scanner;
+// 애너그램 만들기
+public class BOJ_1919 {
+    public static int[] getAlphabetCountArray(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i) - 'a']++;
+        return count;
+    }
+
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        int[] countA = getAlphabetCountArray(a);
+        int[] countB = getAlphabetCountArray(b);
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++)
+            ans += Math.abs(countA[i] - countB[i]);
+        System.out.println(ans);
+    }
+}
